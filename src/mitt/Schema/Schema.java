7b4d28061a8f9a9cf35404ffe47e3470dde3792d
Patrik Greco <sikevux@sikevux.se>
2011-01-14 10:47:45 +0100

    Fills the screen with the schedule

--- src/mitt/Schema/Schema.java
+++ src/mitt/Schema/Schema.java
@@ -9,6 +9,7 @@ import android.widget.ImageView;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import java.util.Calendar;
+import android.view.Display;
 
 public class Schema extends Activity {
 	@Override
@@ -19,11 +20,25 @@ public class Schema extends Activity {
 		SharedPreferences settings = getSharedPreferences("Schema", 0);
 		String schoolId = settings.getString("schoolId", "19400");
 		
-		String site = "http://www.novasoftware.se/ImgGen/schedulegenerator.aspx?schoolid=" + schoolId + "&id=930131-3939&period=&week=" + weekNumber() + "&maxwidth=200&maxheight=500&width=200&height=500";
+		String site = "http://www.novasoftware.se/ImgGen/schedulegenerator.aspx?schoolid=" + schoolId + "&id=930131-3939&period=&week=" + weekNumber() + "&maxwidth=" + maxWidth() + "&maxheight=" + maxHeight() + "&width=" + maxWidth() + "&height="+ maxHeight();
         ImageView imageView = (ImageView) findViewById(R.id.ImageView01);
         Drawable drawable = getIt(site);
         imageView.setImageDrawable(drawable);
     }
+
+	private int maxWidth() {
+		Display sieez = getWindowManager().getDefaultDisplay();
+		int width = sieez.getWidth();
+		return width;
+	}
+	
+	private int maxHeight() {
+		Display sizz = getWindowManager().getDefaultDisplay();
+		int height = sizz.getHeight();
+		height = height - 10;
+		return height;
+	}
+
 	private int weekNumber() {
 		Calendar current = Calendar.getInstance();
 		int week = current.get(Calendar.WEEK_OF_YEAR);
