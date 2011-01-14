b6166d616c12fb13d7cdc8f76ff04a55c3219490
Patrik Greco <sikevux@sikevux.se>
2011-01-14 23:15:21 +0100

    A bunch of stuff

--- src/mitt/Schema/Schema.java
+++ src/mitt/Schema/Schema.java
@@ -8,56 +8,66 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
-import java.util.Calendar;
+import android.text.format.Time;
 import android.view.Display;
 
 public class Schema extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
 
 		SharedPreferences settings = getSharedPreferences("Schema", 0);
 		String schoolId = settings.getString("schoolId", "19400");
-		
-		String site = "http://www.novasoftware.se/ImgGen/schedulegenerator.aspx?schoolid=" + schoolId + "&id=930131-3939&period=&week=" + weekNumber() + "&maxwidth=" + maxWidth() + "&maxheight=" + maxHeight() + "&width=" + maxWidth() + "&height="+ maxHeight();
-        ImageView imageView = (ImageView) findViewById(R.id.ImageView01);
-        Drawable drawable = getIt(site);
-        imageView.setImageDrawable(drawable);
-    }
+		String id = settings.getString("id", "930131-3939");
+		String password = settings.getString("password", "|h%C3%B6st*********");
+		String period = settings.getString("period", "2");
+
+		StringBuilder siteStringBuilder = new StringBuilder("http://www.novasoftware.se/ImgGen/schedulegenerator.aspx?schoolid=");
+		siteStringBuilder.append(schoolId);
+		siteStringBuilder.append("&id=");
+		siteStringBuilder.append(id);
+		siteStringBuilder.append(password);
+		siteStringBuilder.append("&period=");
+		siteStringBuilder.append(period);
+		siteStringBuilder.append("&week=");
+		siteStringBuilder.append("&maxwidth=");
+		siteStringBuilder.append(maxWidth());
+		siteStringBuilder.append("&maxheight=");
+		siteStringBuilder.append(maxHeight());
+		siteStringBuilder.append("&width=");
+		siteStringBuilder.append(maxWidth());
+		siteStringBuilder.append("&height=");
+		siteStringBuilder.append(maxHeight());
+
+		String site = siteStringBuilder.toString();
+		ImageView imageView = (ImageView) findViewById(R.id.ImageView01);
+		Drawable drawable = getIt(site);
+		imageView.setImageDrawable(drawable);
+	}
 
 	private int maxWidth() {
 		Display sieez = getWindowManager().getDefaultDisplay();
 		int width = sieez.getWidth();
 		return width;
 	}
-	
+
 	private int maxHeight() {
 		Display sizz = getWindowManager().getDefaultDisplay();
 		int height = sizz.getHeight();
-		height = height - 10;
 		return height;
 	}
 
-	private int weekNumber() {
-		Calendar current = Calendar.getInstance();
-		int week = current.get(Calendar.WEEK_OF_YEAR);
-		week = week - 1;
-		return week;
-	}
+	private Drawable getIt(String url) {
+		try {
+			InputStream inputStream = (InputStream) new URL(url).getContent();
+			Drawable draw = Drawable.createFromStream(inputStream, "src name");
+			return draw;
+		}
 
-    private Drawable getIt(String url)
-    {
-	try
-	    {
-		InputStream inputStream = (InputStream) new URL(url).getContent();
-		Drawable draw = Drawable.createFromStream(inputStream, "src name");
-		return draw;
-	    } 
-	catch (Exception exception) {
-		System.out.println("Exc="+exception);
-		return null;
+		catch (Exception exception) {
+			System.out.println("Exc="+exception);
+			return null;
+		}
 	}
-    }
-
 }
