83ae93da7b0409d5944f7d54bc3011431c0977af
Patrik Greco <sikevux@sikevux.se>
2011-01-14 01:21:00 +0100

    Stores schoolID in Prefs

--- src/mitt/Schema/Schema.java
+++ src/mitt/Schema/Schema.java
@@ -6,29 +6,38 @@ import android.app.Activity;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 
 public class Schema extends Activity {
-    @Override
+	private static final String TAG = "Schema";
+	@Override
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        ImageView imageView =(ImageView)findViewById(R.id.ImageView01);
-        Drawable drawable = getIt("http://www.novasoftware.se/ImgGen/schedulegenerator.aspx?schoolid=19400&id=930131-3939|h%C3%B6stkyla&period=&week=3&maxwidth=545&maxheight=253&width=545&height=253");
+		SharedPreferences settings = getSharedPreferences("schoolId", 0);
+		String schoolId = settings.getString("schoolId", "19400");
+		
+		String site = "http://www.novasoftware.se/ImgGen/schedulegenerator.aspx?schoolid=" + schoolId + "&id=930131-3939&period=&week=3&maxwidth=545&maxheight=253&width=545&height=253";
+        ImageView imageView = (ImageView) findViewById(R.id.ImageView01);
+        Drawable drawable = getIt(site);
         imageView.setImageDrawable(drawable);
-
+		
     }
 
     private Drawable getIt(String url)
     {
 	try
 	    {
-		InputStream is = (InputStream) new URL(url).getContent();
-		Drawable d = Drawable.createFromStream(is, "src name");
-		return d;
-	    }catch (Exception e) {
-	    System.out.println("Exc="+e);
-	    return null;
+		InputStream inputStream = (InputStream) new URL(url).getContent();
+		Drawable draw = Drawable.createFromStream(inputStream, "src name");
+		return draw;
+	    } 
+	catch (Exception exception) {
+		System.out.println("Exc="+exception);
+		return null;
 	}
     }
+
 }
